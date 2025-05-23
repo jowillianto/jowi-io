513178a4c103dc563af03fcdcdc7ad11ae2a2117
Jonathan Willianto <jowillianto@gmail.com>
2025-05-23 12:17:15 +0900

    [#main] iteratively read instead of recursively read

--- src/reader_writer/str_reader.cc
+++ src/reader_writer/str_reader.cc
@@ -11,26 +11,24 @@ import :error;
 namespace moderna::io {
   export struct str_reader {
     using result_type = std::string;
-    size_t read_count = -1;
+    const size_t read_count = -1;
     std::expected<std::string, fs_error> operator()(const is_basic_file auto &file) const {
       std::string buffer;
       auto buffer_inserter = std::back_inserter(buffer);
-      return (*this)(file, buffer_inserter, read_count).transform([&]() {
-        return std::move(buffer);
-      });
-    }
-    std::expected<void, fs_error> operator()(
-      const is_basic_file auto &file, std::back_insert_iterator<std::string> &it, size_t amount_left
-    ) const {
-      auto reader = basic_reader<4096>{amount_left};
-      return reader(file).and_then([&](auto &&read_res) {
-        std::ranges::copy_n(read_res.buffer.begin(), read_res.size, it);
-        if (read_res.size == 4096) {
-          return (*this)(file, it, amount_left - 4096);
-        } else {
-          return std::expected<void, fs_error>{};
+      size_t amount_left = read_count;
+      while (amount_left > 0) {
+        auto reader = basic_reader<4096>(amount_left);
+        auto read_res = basic_reader<4096>{amount_left}(file);
+        if (!read_res) { return std::unexpected{std::move(read_res.error())}; }
+        std::ranges::copy_n(read_res -> buffer.begin(), read_res -> size, buffer_inserter);
+        if (read_res -> size == 4096) {
+          amount_left -= 4096;
         }
-      });
+        else {
+          amount_left = 0;
+        }
+      }
+      return std::move(buffer);
     }
   };
 }
