fc97374bc4b72d1f46770710a93d6a54dd2f042e
Jonathan Willianto <jowillianto@gmail.com>
2025-09-25 12:56:46 +0900

    [#main] Add local socket coverage to sock tests

--- tests/sock.cc
+++ tests/sock.cc
@@ -105,3 +105,136 @@ JOWI_ADD_TEST(test_connect_and_check_have_data) {
   test_lib::assert_true(test_lib::assert_expected_value(connection.is_readable()));
   fut.wait();
 }
+
+JOWI_ADD_TEST(test_create_local_listener) {
+  auto tmp_dir = std::filesystem::temp_directory_path();
+  auto path = (tmp_dir / ("jowi_local_sock_" + test_lib::random_string(16))).string();
+  test_lib::assert_true(path.length() < 107);
+  std::error_code ec;
+  std::filesystem::remove(path, ec);
+  auto addr = io::local_address::empty();
+  addr.addr(generic::fixed_string<107>{path});
+  auto sock =
+    test_lib::assert_expected_value(io::sock_options{addr}.create().and_then([](auto &&sock) {
+      return std::move(sock).bind_listen(50);
+    }));
+  (void)sock;
+  std::filesystem::remove(path, ec);
+}
+
+JOWI_ADD_TEST(test_local_connect_to_listener) {
+  auto tmp_dir = std::filesystem::temp_directory_path();
+  auto path = (tmp_dir / ("jowi_local_sock_" + test_lib::random_string(16))).string();
+  test_lib::assert_true(path.length() < 107);
+  std::error_code ec;
+  std::filesystem::remove(path, ec);
+
+  auto listener_addr = io::local_address::empty();
+  listener_addr.addr(generic::fixed_string<107>{path});
+  auto listener =
+    test_lib::assert_expected_value(io::sock_options{listener_addr}.create().and_then([](auto &&sock) {
+      return std::move(sock).bind_listen(50);
+    }));
+
+  auto msg = test_lib::random_string(100);
+  auto fut = std::async(
+    std::launch::async,
+    [](auto listener, auto msg) {
+      auto conn = test_lib::assert_expected_value(listener.accept());
+      test_lib::assert_expected_value(conn.write(msg));
+    },
+    std::move(listener),
+    std::string{msg}
+  );
+
+  auto conn_addr = io::local_address::empty();
+  conn_addr.addr(generic::fixed_string<107>{path});
+  auto connection = test_lib::assert_expected_value(
+    io::sock_options{conn_addr}.create().and_then(&io::local_sock_free::connect)
+  );
+
+  io::fixed_buffer<100> recv_msg;
+  test_lib::assert_expected_value(connection.read(recv_msg));
+  test_lib::assert_equal(recv_msg.read_buf(), msg);
+  fut.wait();
+
+  std::filesystem::remove(path, ec);
+}
+
+JOWI_ADD_TEST(test_local_connect_and_check_no_data) {
+  auto tmp_dir = std::filesystem::temp_directory_path();
+  auto path = (tmp_dir / ("jowi_local_sock_" + test_lib::random_string(16))).string();
+  test_lib::assert_true(path.length() < 107);
+  std::error_code ec;
+  std::filesystem::remove(path, ec);
+
+  auto listener_addr = io::local_address::empty();
+  listener_addr.addr(generic::fixed_string<107>{path});
+  auto listener =
+    test_lib::assert_expected_value(io::sock_options{listener_addr}.create().and_then([](auto &&sock) {
+      return std::move(sock).bind_listen(50);
+    }));
+
+  std::promise<void> release_promise;
+  auto release_future = release_promise.get_future();
+  auto fut = std::async(
+    std::launch::async,
+    [](auto listener, std::future<void> release) {
+      auto conn = test_lib::assert_expected_value(listener.accept());
+      release.wait();
+      (void)conn;
+    },
+    std::move(listener),
+    std::move(release_future)
+  );
+
+  auto conn_addr = io::local_address::empty();
+  conn_addr.addr(generic::fixed_string<107>{path});
+  auto connection = test_lib::assert_expected_value(
+    io::sock_options{conn_addr}.create().and_then(&io::local_sock_free::connect)
+  );
+
+  test_lib::assert_false(test_lib::assert_expected_value(connection.is_readable()));
+  release_promise.set_value();
+  fut.wait();
+
+  std::filesystem::remove(path, ec);
+}
+
+JOWI_ADD_TEST(test_local_connect_and_check_have_data) {
+  auto tmp_dir = std::filesystem::temp_directory_path();
+  auto path = (tmp_dir / ("jowi_local_sock_" + test_lib::random_string(16))).string();
+  test_lib::assert_true(path.length() < 107);
+  std::error_code ec;
+  std::filesystem::remove(path, ec);
+
+  auto listener_addr = io::local_address::empty();
+  listener_addr.addr(generic::fixed_string<107>{path});
+  auto listener =
+    test_lib::assert_expected_value(io::sock_options{listener_addr}.create().and_then([](auto &&sock) {
+      return std::move(sock).bind_listen(50);
+    }));
+
+  auto msg = test_lib::random_string(100);
+  auto fut = std::async(
+    std::launch::async,
+    [](auto listener, auto msg) {
+      auto conn = test_lib::assert_expected_value(listener.accept());
+      test_lib::assert_expected_value(conn.write(msg));
+    },
+    std::move(listener),
+    std::string{msg}
+  );
+
+  auto conn_addr = io::local_address::empty();
+  conn_addr.addr(generic::fixed_string<107>{path});
+  auto connection = test_lib::assert_expected_value(
+    io::sock_options{conn_addr}.create().and_then(&io::local_sock_free::connect)
+  );
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  test_lib::assert_true(test_lib::assert_expected_value(connection.is_readable()));
+  fut.wait();
+
+  std::filesystem::remove(path, ec);
+}
